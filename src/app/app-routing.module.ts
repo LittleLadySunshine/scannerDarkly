335d74eb2f12b412e97fbb27cc1f625ec8da0adc
Denise McCoy <Denise.L.McCoy@gmail.com>
2019-06-06 16:13:57 -0600

    added some style using CSS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+import { ScansComponent } from './scans/scans.component';
+import { ScanDetailComponent } from './scan-detail/scan-detail.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileDetailComponent } from './profile-detail/profile-detail.component';
+import { ReportComponent } from './report/report.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/scans', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'scans', component: ScansComponent },
+  { path: 'scans/:id', component: ScansComponent },
+  { path: 'profiles', component: ProfileComponent },
+  { path: 'profiles/:id', component: ProfileComponent },
+  { path: 'report/:id', component: ReportComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
